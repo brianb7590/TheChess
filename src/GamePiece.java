609b07cd74ca9258de45c8fe162bc65d0decad94
Brian B <brianb7590@gmail.com>
2014-12-08 22:11:40 -0500

    Initial Commit GamePiece interface

---
+++ src/GamePiece.java
@@ -0,0 +1,14 @@
+/**
+ * Created by brianbenavides on 12/8/14.
+ */
+public interface GamePiece {
+
+    public boolean canMove();
+
+    public void moveTo(int x, int y);
+    
+    public boolean getName();
+
+    public boolean getId();
+
+}
